72eed5f543302a677851306b2d625cfa3ffd3021
juhee-dev <viimv2000@gmail.com>
2022-10-07 21:10:23 +0900

    깃허브 생성

---
+++ app/src/main/java/com/yuwol/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.yuwol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //유월 팀 프론트엔드 깃허브!!
+    }
+}
