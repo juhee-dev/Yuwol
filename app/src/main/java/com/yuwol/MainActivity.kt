58a15609ddc681300d6301e330b049f682892a41
cindykwak <cindykwak0809@gmail.com>
2022-10-07 21:44:16 +0900

    글자를 썼어222

--- app/src/main/java/com/yuwol/MainActivity.kt
+++ app/src/main/java/com/yuwol/MainActivity.kt
@@ -8,7 +8,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //유월 팀 프론트엔드 깃허브!!!!
+        //유월 팀 프론트엔드 깃허브g!!!!
         // 요건 내가 쓴거야 -주아!!!!
+        //다시 시도-주아
     }
 }
