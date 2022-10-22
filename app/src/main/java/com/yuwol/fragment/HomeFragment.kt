1264b41a14025c5d772ebf3b112f728e52a170e3
Juhee <viimv2000@gmail.com>
2022-10-23 04:57:27 +0900

    Horizontal recyclerView

--- app/src/main/java/com/yuwol/fragment/HomeFragment.kt
+++ app/src/main/java/com/yuwol/fragment/HomeFragment.kt
@@ -1,12 +1,13 @@
 package com.yuwol.fragment
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.yuwol.R
 import com.yuwol.adapter.ChartAdapter
 import com.yuwol.databinding.FragmentHomeBinding
@@ -32,6 +33,8 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         Log.d("fragment", "HomeFragment")
+
+        binding.rvChart.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
 
         // 우리가 사용할 어댑터의 초기값을 넣어줌
         chartAdapter = ChartAdapter()
