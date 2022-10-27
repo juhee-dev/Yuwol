8ae11c6f59153279902937075bed0839db2f736a
Juhee <viimv2000@gmail.com>
2022-10-27 21:30:11 +0900

    HomeFragment.kt 코드 함수로 캡슐화

--- app/src/main/java/com/yuwol/adapter/ChartAdapter.kt
+++ app/src/main/java/com/yuwol/adapter/ChartAdapter.kt
@@ -3,25 +3,25 @@ package com.yuwol.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.yuwol.databinding.ListItemChartBinding
+import com.yuwol.databinding.ItemHomeChartBinding
 import com.yuwol.model.Chart
 
 class ChartAdapter : RecyclerView.Adapter<ChartAdapter.ViewHolder>() {
     // Adapter는 ViewHolder로 변경할 데이터를 가지고 있어야 함
-    val chartList = mutableListOf<Chart>()
+    var dataList = mutableListOf<Chart>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ListItemChartBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+        val binding = ItemHomeChartBinding.inflate(LayoutInflater.from(parent.context),parent, false)
         return ViewHolder(binding)
     }
     // Adapter는 ViewHolder에 Data를 전달하는 방법을 정의해야 함
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.onBind(chartList[position])
+        holder.onBind(dataList[position])
     }
     // Adapter는 전체 아이템의 수를 알아야 함
-    override fun getItemCount(): Int = chartList.size
+    override fun getItemCount(): Int = dataList.size
 
-    class ViewHolder(private val binding: ListItemChartBinding) : RecyclerView.ViewHolder(binding.root){
+    class ViewHolder(private val binding: ItemHomeChartBinding) : RecyclerView.ViewHolder(binding.root){
         fun onBind(chart: Chart){
             binding.ivChartCover.setImageResource(chart.cover)
             binding.tvChartTitle.text = chart.title
