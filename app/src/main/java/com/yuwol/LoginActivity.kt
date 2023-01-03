f5ba16098e32090c24d6e8e8c0fb3feedc1c3cfe
Juhee <viimv2000@gmail.com>
2023-01-03 21:57:10 +0900

    feat: 회원가입-프로필 이미지 등록 기능

--- app/src/main/java/com/yuwol/LoginActivity.kt
+++ app/src/main/java/com/yuwol/LoginActivity.kt
@@ -39,19 +39,14 @@ class LoginActivity : AppCompatActivity() {
                     SucceedKakaoLogin()
                 }
             }
-            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+
             if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
                 UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
                     if (error != null) {
                         Log.e(TAG, "카카오톡으로 로그인 실패", error)
-
-                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
                         if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
                             return@loginWithKakaoTalk
                         }
-
-                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
                         UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
                     } else if (token != null) {
                         Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
@@ -65,7 +60,6 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun SucceedKakaoLogin() {
-
         UserApiClient.instance.me { user, error ->
             if (error != null) {
                 Log.e(TAG, "사용자 정보 요청 실패", error)
@@ -87,11 +81,18 @@ class LoginActivity : AppCompatActivity() {
                             Log.d(TAG, "kakao login onResponse: success")
                             Log.d(TAG, "data value: ${requestLoginData}")
                             val data = response.body()?.accessToken
-                            Log.d(TAG, "response value: ${data.toString()}")
-
+                            Log.d(TAG, "response value - token: ${data.toString()}")
                             // 로그인 성공
-                            if (data != null) {
-                                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                            val isFirst = response.body()?.isFirst
+                            Log.d(TAG, "response value - isFirst: ${isFirst.toString()}")
+                            if (isFirst != null) {
+                                val intent: Intent
+                                if (isFirst == true) {
+                                    intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+                                } else {
+//                                    intent = Intent(this@LoginActivity, MainActivity::class.java)
+                                    intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+                                }
                                 startActivity(intent)
                                 finish()
                             } else {
