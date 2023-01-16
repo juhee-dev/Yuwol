36a30906a746902d5d2966735cd87d6750ce2a8f
Juhee <viimv2000@gmail.com>
2023-01-16 20:13:47 +0900

    feat: 멜론차트 구현중, activity->fragment 값 전달 완료

--- app/src/main/java/com/yuwol/data/response/ResponseMelonChartData.kt
+++ app/src/main/java/com/yuwol/data/response/ResponseMelonChartData.kt
@@ -1,22 +1,11 @@
-//package com.yuwol.data.response
-//
-//import android.os.Parcelable
-//import kotlinx.parcelize.Parcelize
-//
-//data class ResponseMelonChartData(
-//
-//    val songld: String,
-//    val title: String,
-//    val singer: String,
-//    val imageUrl: String,
-//    val totalDifficult: String,
-//    var mood: String,
-//    var isMySinglist: String
-//
-//
-//):Parcelable
-//@Parcelize
-//class content(
-//    val status: String,
-//
-//)
+package com.yuwol.data.response
+
+data class ResponseMelonChartData(
+    val songld: String,
+    val title: String,
+    val singer: String,
+    val imageUrl: String,
+    val totalDifficult: String,
+    var mood: String,
+    var isMySinglist: String
+)
