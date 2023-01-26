94340b8fe472ca2601802722fa3ef86c7056d1f8
Juhee <viimv2000@gmail.com>
2023-01-26 14:08:28 +0900

    style: fragment_user.xml 디자인 수정

--- app/src/main/java/com/yuwol/fragment/UserFragment.kt
+++ app/src/main/java/com/yuwol/fragment/UserFragment.kt
@@ -11,10 +11,7 @@ import androidx.core.text.set
 import androidx.core.text.toSpannable
 import com.yuwol.LinearGradientSpan
 import com.yuwol.R
-import com.yuwol.adapter.RateAdapter
-import com.yuwol.databinding.FragmentFirstratingBinding
 import com.yuwol.databinding.FragmentUserBinding
-import com.yuwol.model.Rate
 
 class UserFragment : Fragment() {
     lateinit var binding: FragmentUserBinding
@@ -31,13 +28,12 @@ class UserFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setTitleGradient("나의\n싱리스트", binding.textView3)
-        setTitleGradient("나의\n관심", binding.textView4)
-        setTitleGradient("나의\n평가", binding.textView5)
-        setTitleGradient("프로필\n수정", binding.textView6)
-        setTitleGradient("설정", binding.textView7)
-
-
+        setTitleGradient(binding.textView.text.toString(), binding.textView)
+        setSettingsGradient(binding.textView3.text.toString(), binding.textView3)
+        setSettingsGradient(binding.textView4.text.toString(), binding.textView4)
+        setSettingsGradient(binding.textView5.text.toString(), binding.textView5)
+        setSettingsGradient(binding.textView6.text.toString(), binding.textView6)
+        setSettingsGradient(binding.textView7.text.toString(), binding.textView7)
 
         binding.ivRectangle.setOnClickListener {
             parentFragmentManager.beginTransaction().replace(R.id.fl_main, MySinglistFragment())
@@ -62,9 +58,16 @@ class UserFragment : Fragment() {
         }
 
     }
+
     private fun setTitleGradient(text: String, tv: TextView) {
         val spannable = text.toSpannable()
-        spannable[0..text.length] = LinearGradientSpan(text, text, ContextCompat.getColor(requireContext(), R.color.pink_100), ContextCompat.getColor(requireContext(), R.color.purple_100))
+        spannable[0..text.length] = LinearGradientSpan(text, text, ContextCompat.getColor(requireContext(), R.color.pink_300), ContextCompat.getColor(requireContext(), R.color.purple_300))
+        tv.text = spannable
+    }
+
+    private fun setSettingsGradient(text: String, tv: TextView) {
+        val spannable = text.toSpannable()
+        spannable[0..text.length] = LinearGradientSpan(text, text, ContextCompat.getColor(requireContext(), R.color.pink_400), ContextCompat.getColor(requireContext(), R.color.purple_100))
         tv.text = spannable
     }
 
